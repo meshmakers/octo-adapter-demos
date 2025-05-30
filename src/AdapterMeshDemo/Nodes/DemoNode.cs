57e0c25ffe7449369b97ef77bcc135b06a478fe4
Markus Roider <markus.roider@meshmakers.io>
2025-05-30 17:23:37 +0200

    Adapter tests

--- src/AdapterMeshDemo/Nodes/DemoNode.cs
+++ src/AdapterMeshDemo/Nodes/DemoNode.cs
@@ -1,6 +1,7 @@
 ﻿using Meshmakers.Octo.Sdk.Common.EtlDataPipeline;
 using Meshmakers.Octo.Sdk.Common.EtlDataPipeline.Configuration;
 using Meshmakers.Octo.Sdk.Common.EtlDataPipeline.Nodes;
+using Newtonsoft.Json.Linq;
 
 namespace Meshmakers.Octo.Communication.MeshAdapter.Demo.Nodes;
 
@@ -9,7 +10,7 @@ namespace Meshmakers.Octo.Communication.MeshAdapter.Demo.Nodes;
 /// </summary>
 [NodeName("Demo", 1)]
 public record
-    DemoNodeConfiguration : TargetPathNodeConfiguration // Alternatives are:
+    DemoNodeConfiguration : SourceTargetPathNodeConfiguration // Alternatives are:
                                                         // TargetPathNodeConfiguration (defines TargetPath and options),
                                                         // PathNodeConfiguration (defines Path),
                                                         // SourceTargetPathNodeConfiguration (defines Path + TargetPath and options)
@@ -37,19 +38,19 @@ public class DemoNode(
 
         nodeContext.Info("DemoNode: " + c.MyMessage);
 
-        var data = dataContext.Current;
-        var out1 = data?.Value<string>("out");
-        var out2 = c.MyMessage;
-        if (out1 != null)
+        var pathData = dataContext.Current?.SelectToken(c.Path) as JValue;
+        var input= pathData?.Value<string>();
+        
+        var output = c.MyMessage;
+        if (input != null)
         {
-            out2 = out1 + " " + out2;
+            output = input + " " + output;
         }
         
         // set value
         dataContext.SetValueByPath(c.TargetPath, c.DocumentMode, c.TargetValueKind, c.TargetValueWriteMode,
-            out2);
+            output);
 
-        await Task.Delay(1000);
         // Continue with next node in pipeline
         await next(dataContext, nodeContext);
     }
